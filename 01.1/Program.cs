692f060ce5d743aa16e2a1c293b8d35855d5132b
LauwiMeara <laurasusanne@live.nl>
2021-12-01 08:06:12 +0100

    Add day 1

---
+++ 01.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] report = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+            int numIncreases = 0;
+
+            for (int i = 1; i < report.Length; i++)
+            {
+                if (report[i] > report [i - 1]) numIncreases++;
+            }
+
+            Console.WriteLine($"The number of increases is {numIncreases}");
+        }
+    }
+}
---
+++ 01.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] report = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+            
+            int numIncreases = 0;
+            int firstSum = report[0] + report[1] + report[2];
+
+            for (int i = 1; i < report.Length - 2; i++)
+            {
+                int secondSum = report[i] + report[i + 1] + report[i + 2];
+                if(secondSum > firstSum) numIncreases++;
+                firstSum = secondSum;
+            }
+
+            Console.WriteLine($"The number of increases is {numIncreases}");
+        }
+    }
+}
