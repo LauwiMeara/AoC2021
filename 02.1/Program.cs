421512db547a704773bfb3f2d81c4745620d6e82
LauwiMeara <laurasusanne@live.nl>
2021-12-02 07:25:20 +0100

    Add day 2

---
+++ 02.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[][] course = File.ReadAllLines("input.txt").Select(command => command.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+            int depth = 0;
+            int horizontalPosition = 0;
+
+            foreach (string[] command in course)
+            {
+                switch (command[0])
+                {
+                    case "forward":
+                        horizontalPosition += int.Parse(command[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(command[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(command[1]);
+                        break;
+                }
+            }
+
+            int product = depth * horizontalPosition;
+
+            Console.WriteLine($"The depth multiplied by the horizontal position is {product}");
+        }
+    }
+}
---
+++ 02.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[][] course = File.ReadAllLines("input.txt").Select(command => command.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+            int depth = 0;
+            int horizontalPosition = 0;
+            int aim = 0;
+
+            foreach (string[] command in course)
+            {
+                switch (command[0])
+                {
+                    case "forward":
+                        horizontalPosition += int.Parse(command[1]);
+                        depth += aim * int.Parse(command[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(command[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(command[1]);
+                        break;
+                }
+            }
+
+            int product = depth * horizontalPosition;
+
+            Console.WriteLine($"The depth multiplied by the horizontal position is {product}");
+        }
+    }
+}
