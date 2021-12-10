a1b86a6e65d754e3b9370ab1965285f515458d16
LauwiMeara <laurasusanne@live.nl>
2021-12-10 19:26:42 +0100

    Add day 10

---
+++ 10.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _10._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            char[] openingChunks = new char[] { '(', '[', '{', '<' };
+            Dictionary<char, int> illegalChunks = new Dictionary<char, int>
+            {
+                { ')', 0 },
+                { ']', 0 },
+                { '}', 0 },
+                { '>', 0 }
+            };
+
+            foreach (string line in input)
+            {
+                List<char> chunks = new List<char>
+                {
+                    line[0]
+                };
+
+                for (int i = 1; i < line.Length; i++)
+                {
+                    char chunk = line[i];
+
+                    if (openingChunks.Contains(chunk))
+                    {
+                        chunks.Add(chunk);
+                        continue;
+                    }
+
+                    char prevChunk = chunks.Last();
+
+                    if ((prevChunk == '(' && chunk == ')') ||
+                        (prevChunk == '[' && chunk == ']') ||
+                        (prevChunk == '{' && chunk == '}') ||
+                        (prevChunk == '<' && chunk == '>'))
+                    {
+                        chunks.RemoveAt(chunks.Count - 1);
+                        continue;
+                    }
+
+                    illegalChunks[chunk]++;
+                    break;
+                }
+            }
+
+            int score = (illegalChunks[')'] * 3) + (illegalChunks[']'] * 57) + (illegalChunks['}'] * 1197) + (illegalChunks['>'] * 25137);
+            Console.WriteLine($"My score is {score}!");
+        }
+    }
+}
---
+++ 10.2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _10._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            char[] openingChunks = new char[] { '(', '[', '{', '<' };
+            List<long> scores = new List<long>();
+
+            foreach (string line in input)
+            {
+                bool corrupted = false;
+
+                List<char> chunks = new List<char>
+                {
+                    line[0]
+                };
+
+                for (int i = 1; i < line.Length; i++)
+                {
+                    char chunk = line[i];
+
+                    if (openingChunks.Contains(chunk))
+                    {
+                        chunks.Add(chunk);
+                        continue;
+                    }
+
+                    char prevChunk = chunks.Last();
+
+                    if ((prevChunk == '(' && chunk == ')') ||
+                        (prevChunk == '[' && chunk == ']') ||
+                        (prevChunk == '{' && chunk == '}') ||
+                        (prevChunk == '<' && chunk == '>'))
+                    {
+                        chunks.RemoveAt(chunks.Count - 1);
+                        continue;
+                    }
+
+                    corrupted = true;
+                    break;
+                }
+
+                if (!corrupted)
+                {
+                    long subScore = 0;
+
+                    while (chunks.Count != 0)
+                    {
+                        subScore *= 5;
+
+                        switch (chunks.Last())
+                        {
+                            case '(':
+                                subScore += 1;
+                                break;
+                            case '[':
+                                subScore += 2;
+                                break;
+                            case '{':
+                                subScore += 3;
+                                break;
+                            case '<':
+                                subScore += 4;
+                                break;
+                        }
+
+                        chunks.RemoveAt(chunks.Count - 1);
+                    }
+
+                    scores.Add(subScore);
+                }
+            }
+
+            scores.Sort();
+            long totalScore = scores[(scores.Count - 1) / 2];
+
+            Console.WriteLine($"My score is {totalScore}!");
+        }
+    }
+}
