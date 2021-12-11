5d778dec43534ee1278651dd723ed752a88a5b0a
LauwiMeara <laurasusanne@live.nl>
2021-12-11 10:42:01 +0100

    Add day 11

---
+++ 11.2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _11._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[][] input = File.ReadAllLines("input.txt").Select(row => row.Select(energy => int.Parse(energy.ToString())).ToArray()).ToArray();
+            int numOctopuses = input.SelectMany(o => o).Count();
+            bool synchronizedFlash = false;
+            int steps = 0;
+
+            while (!synchronizedFlash)
+            {
+                List<(int, int)> flashed = new List<(int, int)>();
+
+                for (int row = 0; row < input.Length; row++)
+                {
+                    for (int col = 0; col < input[row].Length; col++)
+                    {
+                        input[row][col]++;
+                        
+                        if (input[row][col] > 9 && !flashed.Contains((row, col)))
+                        {
+                            flashed.Add((row, col));
+                            Flash(input, (row, col), flashed);
+                        }
+                    }
+                }
+
+                foreach ((int row, int col) f in flashed)
+                {
+                    input[f.row][f.col] = 0;
+                }
+
+                steps++;
+
+                if (flashed.Count == numOctopuses) synchronizedFlash = true;
+            }
+
+            Console.WriteLine($"The first synchronized flash is at step {steps}");
+        }
+
+        static void Flash(int[][] input, (int row, int col) f, List<(int, int)> flashed)
+        {
+            (int row, int col)[] adjacents = { (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1) };
+
+            foreach ( (int row, int col) a in adjacents)
+            {
+                int row = f.row + a.row;
+                int col = f.col + a.col;
+
+                if (row >= 0 &&
+                    row < input.Length &&
+                    col >= 0 &&
+                    col < input[f.row].Length)
+                {
+                    if (input[row][col] <= 9)
+                    {
+                        input[row][col]++;
+
+                        if (input[row][col] > 9 && !flashed.Contains((row, col)))
+                        {
+                            flashed.Add((row, col));
+                            Flash(input, (row, col), flashed);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
